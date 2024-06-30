3dc4f0e82a8ba59eae2fea9ff74df99fa4b5548d
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2024-06-30 19:45:57 +0900

    [WIP] update test

--- tests/preprocessors_test.py
+++ tests/preprocessors_test.py
@@ -1,10 +1,8 @@
 import copy
-import json
-import pathlib
-import random
 from typing import Type
 
 import numpy as np
+import pandas as pd
 import pytest
 import torch
 
@@ -200,23 +198,102 @@ class TestRefinementProcessor(LayoutPrompterTestCase):
 
 
 class TestContentAwareProcessor(LayoutPrompterTestCase):
+    TASK: Task = "content"
+
+    def load_metadata(self, dataset_name: str) -> pd.DataFrame:
+        metadata_path = self.FIXTURES_ROOT / dataset_name / "metadata_small.csv"
+        return pd.read_csv(metadata_path)
+
     @pytest.mark.parametrize(
         argnames="dataset_config",
         argvalues=(PosterLayoutDatasetConfig(),),
     )
+    @pytest.mark.parametrize(
+        argnames="filenum",
+        argvalues=(0, 1000, 2000, 3000, 4000, 5000),
+    )
     def test_processor(self, dataset_config: LayoutDatasetConfig, filenum: int):
-        raw_data = self.load_raw_data(dataset_name=dataset_config.name, filenum=filenum)
-        # processor = create_processor(dataset_config=, )
-        raise NotImplementedError
+        metadata = self.load_metadata(dataset_name=dataset_config.name)
+
+        processor = create_processor(
+            dataset_config=dataset_config,
+            task=self.TASK,
+            metadata=metadata,
+        )
+        raw_data_path = (
+            self.FIXTURES_ROOT
+            / dataset_config.name
+            / "raw"
+            / f"{filenum}_mask_pred.png"
+        )
+        processed_data = processor(
+            str(raw_data_path),
+            idx=filenum,
+            split="train",
+        )
+        expected_processed_data = self.load_processed_data(
+            dataset_name=dataset_config.name,
+            task=self.TASK,
+            filenum=filenum,
+        )
+        assert set(processed_data.keys()) == set(expected_processed_data.keys())
+
+        for k in processed_data.keys():
+            processed_data_k = processed_data[k]
+            expected_processed_data_k = expected_processed_data[k]
+
+            if isinstance(processed_data_k, torch.Tensor):
+                processed_data_k = processed_data_k.numpy().tolist()
+
+            assert processed_data_k == expected_processed_data_k  # type: ignore
 
 
 class TestTextToLayoutProcessor(LayoutPrompterTestCase):
+    TASK: Task = "text"
+
+    def _convert_raw_to_tensor_dict(self, data):
+        # There is no need to convert the data to tensor in this task
+        return data
+
     @pytest.mark.parametrize(
         argnames="dataset_config",
-        argvalues=(RicoDatasetConfig(), PubLayNetDatasetConfig()),
+        argvalues=(WebUIDatasetConfig(),),
     )
-    def test_processor(self, dataset_config: LayoutDatasetConfig):
-        raise NotImplementedError
+    @pytest.mark.parametrize(
+        argnames="filenum",
+        argvalues=(0, 1000, 2000, 3000, 4000, 5000),
+    )
+    def test_processor(self, dataset_config: LayoutDatasetConfig, filenum: int):
+        raw_data = self.load_raw_data(
+            dataset_name=dataset_config.name,
+            filenum=filenum,
+        )
+        processor = create_processor(
+            dataset_config=dataset_config,
+            task=self.TASK,
+        )
+        processed_data = processor(copy.deepcopy(raw_data))
+
+        expected_processed_data = self.load_processed_data(
+            dataset_name=dataset_config.name,
+            task=self.TASK,
+            filenum=filenum,
+        )
+        assert set(processed_data.keys()) == set(expected_processed_data.keys())
+
+        for k in processed_data.keys():
+            processed_data_k = processed_data[k]
+            expected_processed_data_k = expected_processed_data[k]
+
+            if isinstance(processed_data_k, str):
+                assert processed_data_k == expected_processed_data_k
+            elif isinstance(processed_data_k, torch.Tensor):
+                processed_data_k = processed_data_k.numpy().tolist()
+                assert np.allclose(
+                    processed_data_k, expected_processed_data_k, atol=1e-5
+                )
+            else:
+                raise ValueError(f"Unexpected type (k: {k}): {type(processed_data_k)}")
 
 
 class TestCreateProcessor(LayoutPrompterTestCase):
